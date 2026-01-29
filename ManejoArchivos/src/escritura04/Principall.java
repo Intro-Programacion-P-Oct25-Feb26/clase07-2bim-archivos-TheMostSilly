2b124a87d1c06cbb1056b548d7b51f0e72cb7cb8
TheMostSilly <geovannacastro058@gmail.com>
2026-01-29 11:34:50 -0500

    hola

---
+++ ManejoArchivos/src/escritura04/Principall.java
@@ -0,0 +1,32 @@
+package escritura04;
+
+import escritura01.CrearArchivoTexto;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Principall {
+
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        String cadenaFinal = "";
+
+        for (int i = 1; i < 4; i++) {
+            cadenaFinal = "";
+            System.out.println("Ingrese su cédula");
+            String cedula = entrada.nextLine();
+            System.out.println("Ingrese su nombre");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese su correo");
+            String correo = entrada.nextLine();
+
+            cadenaFinal = String.format("%s%s %s %s\n", cadenaFinal,
+                    cedula,
+                    nombre, correo);
+            Secundaria.agregarRegistros(cadenaFinal, cedula);
+        }
+
+    } // fin de main
+}
